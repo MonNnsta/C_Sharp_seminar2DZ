0717577475ed3048ce71e213b9685368bc4e4eb9
Kirill Plokhotnichenko <www.origin@bk.ru>
2023-03-05 15:32:42 +0500

    TASK15

---
+++ Task15/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+bool WeekendYes(int Weekend)
+{
+    if (Weekend < 5)
+    {
+        return false;
+    }
+    return true;
+}
+
+int Weekend = Promt("Введите № дня недели от 1 до 7 ");
+
+if (WeekendYes(Weekend))
+    Console.WriteLine("Повезло,повезло, это выходной день! ");
+else
+    Console.WriteLine("Пора на завод, работяги! ");
