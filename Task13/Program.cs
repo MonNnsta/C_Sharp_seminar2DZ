026a01d954bafe9dd8a8a3a0bb5c029d606a1690
Kirill Plokhotnichenko <www.origin@bk.ru>
2023-03-05 15:11:48 +0500

    TASK13

---
+++ Task13/Program.cs
@@ -0,0 +1,34 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int ThirdRank(int num)
+{
+    while (num > 999)
+    {
+        num /= 10;
+    }
+    return num % 10;
+}
+
+bool ValidateNum(int num)
+{
+    if (num < 100)
+    {
+        Console.WriteLine("Третьей цифры нет! ");
+        return false;
+    }
+    return true;
+
+}
+
+int num = Prompt("Введите число -> ");
+if (ValidateNum(num))
+{
+
+    Console.WriteLine($"Третья цифра в числе -> {ThirdRank(num)}");
+}
