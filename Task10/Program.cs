b09152063cca7f5d0b3c872f6a42fda0e7e7b640
Kirill Plokhotnichenko <www.origin@bk.ru>
2023-03-05 14:11:18 +0500

    TASK10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите трёхзначное число -> ");
+int num = int.Parse(Console.ReadLine() ?? "");
+
+if (num < 100 || num >= 1000)
+{
+    Console.WriteLine("Вы ввели не трехзначное число, повторите ввод! ");
+    return;    
+}
+
+else
+{
+    Console.WriteLine($"Введенное число {num} ");
+
+    int solutionNum = num / 10 % 10;
+    Console.WriteLine($"Вторая цифра в веденном числе -> {solutionNum} ");
+}
